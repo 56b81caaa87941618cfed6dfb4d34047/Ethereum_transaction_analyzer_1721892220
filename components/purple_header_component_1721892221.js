7013eb81350f202c265c1b57029f9c1214d1bb34
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-07-25 07:23:49 +0000

    Placeholder text replacement

--- components/purple_header_component_1721892221.js
+++ components/purple_header_component_1721892221.js
@@ -15,11 +15,11 @@ Vue.component("purple_header_component_1721892221", {
                 <nav id="desktop-navigation" class="hidden md:flex md:grow">
     
                     <ul id="desktop-menu-links" class="flex grow justify-center flex-wrap items-center">
-                        <li><a id="HEADER_NAVBAR_TEXT_1-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="HEADER_NAVBAR_TEXT_1.html">HEADER_NAVBAR_TEXT_1</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_2-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_2.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_2</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_3-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_3.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_3</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_4-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_4.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_4</a></li>
-                        <li><a id="HEADER_NAVBAR_TEXT_5-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="HEADER_NAVBAR_TEXT_5.html" :class="[textStylePrimary, hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_5</a></li>
+                        <li><a id="Ethereum Transaction Monitor-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" :class="[textStylePrimary, hoverTextStylePrimary]" href="Ethereum Transaction Monitor.html">Ethereum Transaction Monitor</a></li>
+                        <li><a id="Live Tracking-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Live Tracking.html" :class="[textStylePrimary, hoverTextStylePrimary]">Live Tracking</a></li>
+                        <li><a id="Advanced Analytics-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Advanced Analytics.html" :class="[textStylePrimary, hoverTextStylePrimary]">Advanced Analytics</a></li>
+                        <li><a id="Insightful Reports-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Insightful Reports.html" :class="[textStylePrimary, hoverTextStylePrimary]">Insightful Reports</a></li>
+                        <li><a id="Customizable Alerts-link" class="font-medium text-sm mx-4 lg:mx-5 transition duration-150 ease-in-out" href="Customizable Alerts.html" :class="[textStylePrimary, hoverTextStylePrimary]">Customizable Alerts</a></li>
                     </ul>
     
                 </nav>
@@ -42,11 +42,11 @@ Vue.component("purple_header_component_1721892221", {
     
                     <nav id="mobile-nav" class="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out" x-ref="mobileNav" :style="expanded ? 'max-height: ' + $refs.mobileNav.scrollHeight + 'px; opacity: 1' : 'max-height: 0; opacity: .8'" @click.outside="expanded = false" @keydown.escape.window="expanded = false" x-cloak>
                         <ul id="mobile-nav-list" class="border border-transparent rounded-lg px-4 py-1.5" :class="signUpButtonGradient">
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_1-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_1.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_1</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_2-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_2.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_2</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_3-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_3.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_3</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_4-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_4.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_4</a></li>
-                            <li><a id="mobile-HEADER_NAVBAR_TEXT_5-link" class="flex font-medium text-sm py-1.5" href="HEADER_NAVBAR_TEXT_5.html" :class="[textStylePrimary,hoverTextStylePrimary]">HEADER_NAVBAR_TEXT_5</a></li>
+                            <li><a id="mobile-Ethereum Transaction Monitor-link" class="flex font-medium text-sm py-1.5" href="Ethereum Transaction Monitor.html" :class="[textStylePrimary,hoverTextStylePrimary]">Ethereum Transaction Monitor</a></li>
+                            <li><a id="mobile-Live Tracking-link" class="flex font-medium text-sm py-1.5" href="Live Tracking.html" :class="[textStylePrimary,hoverTextStylePrimary]">Live Tracking</a></li>
+                            <li><a id="mobile-Advanced Analytics-link" class="flex font-medium text-sm py-1.5" href="Advanced Analytics.html" :class="[textStylePrimary,hoverTextStylePrimary]">Advanced Analytics</a></li>
+                            <li><a id="mobile-Insightful Reports-link" class="flex font-medium text-sm py-1.5" href="Insightful Reports.html" :class="[textStylePrimary,hoverTextStylePrimary]">Insightful Reports</a></li>
+                            <li><a id="mobile-Customizable Alerts-link" class="flex font-medium text-sm py-1.5" href="Customizable Alerts.html" :class="[textStylePrimary,hoverTextStylePrimary]">Customizable Alerts</a></li>
                         </ul>
                     </nav>
     
